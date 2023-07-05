4e4dd2baff11054ab8046c053007cc97f59dd29f
rupan.chaulagain <rupanchaulagain.rc@gmail.com>
2023-07-05 11:33:15 +0545

    Fix static rendering for api endpoint to prevent dynamic error and refactor code for server side

--- client/src/app/api/db/route.ts
+++ client/src/app/api/db/route.ts
@@ -1,23 +1,19 @@
 import {getAddressDetails, getPoolDetails, getTheLatestTransactionEpochOfAddress} from "@app/db/queries";
 import {NextResponse} from "next/server";
 import {encode} from "cbor-x";
-import {convertToTableData, getLatestEpoch} from "@app/utils/cardano-utils";
+import {convertToTableData, getLatestEpoch, getUrlObject} from "@app/utils/cardano-utils";
 
 
-function getUrlObject(urlStr: string) {
-    return new URL(urlStr);
-}
+
 
 async function getTransactionHistoryOfPool(id: string, pageNumber: number) {
     const latestEpoch = await getTheLatestTransactionEpochOfAddress(id);
-    const data = await getPoolDetails(id, latestEpoch, pageNumber);
-    return convertToTableData(data);
+    return await getPoolDetails(id, latestEpoch, pageNumber);
 }
 
 async function getTransactionHistoryOfAddress(id: string, pageNumber: number) {
     const latestEpoch = getLatestEpoch();
-    const data = await getAddressDetails(id, latestEpoch, pageNumber)
-    return convertToTableData(data);
+    return await getAddressDetails(id, latestEpoch, pageNumber)
 }
 
 
@@ -26,13 +22,18 @@ export async function GET(req: Request) {
     const id = urlObject.searchParams.get("id") as string;
     const pageNumber = parseInt(<string>urlObject.searchParams.get("pageNumber"));
     let data;
-    if (id.startsWith("pool")) {
-        data = await getTransactionHistoryOfPool(id, pageNumber)
-    } else {
-        data = await getTransactionHistoryOfAddress(id, pageNumber)
+    try {
+        if (id.startsWith("pool")) {
+            data = await getTransactionHistoryOfPool(id, pageNumber)
+        } else {
+            data = await getTransactionHistoryOfAddress(id, pageNumber)
+        }
+        const serializedBuffer = encode(convertToTableData(data));
+        const response = new NextResponse(serializedBuffer);
+        response.headers.set("Content-Type", "application/cbor")
+        return response;
+    } catch (e: any) {
+        console.log("/api/db/", e);
+        return NextResponse.json({error: e.name, status: !e?.errorCode ? 500 : e.errorCode})
     }
-    const serializedBuffer = encode(data);
-    const response = new NextResponse(serializedBuffer);
-    response.headers.set("Content-Type", "application/cbor")
-    return response;
 }
