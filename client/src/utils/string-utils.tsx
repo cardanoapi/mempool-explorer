7cd52e4141253010f171fb95beed824543bab66f
rupan.chaulagain <rupanchaulagain.rc@gmail.com>
2023-07-12 16:06:50 +0545

    Remove unused components and code refactors

--- client/src/utils/string-utils.tsx
+++ client/src/utils/string-utils.tsx
@@ -1,17 +1,16 @@
-export const toMidDottedStr = (str: string | number, leadingVisible = 12, firstIndex = 0) => {
-    if (str === undefined) return str;
+export const toMidDottedStr = (str: string, leadingVisible = 12, firstIndex = 0) => {
+    if (str === undefined || str.length < 12) return str;
     const total = str.toString().length;
     const leadingStr = str.toString().substring(firstIndex, leadingVisible);
     const trailingStr = str.toString().substring(total - leadingVisible);
     return `${leadingStr}...${trailingStr}`;
 };
 
-
-export const Heading = (props:any) => {
+export const Heading = (props: any) => {
     return (
-        <div className={"flex justify-between items-center"} style={{justifyContent:"space-between"}}>
-            <h1 className={"font-bold mb-2 px-2 text-lg"}>{props.title}</h1>
+        <div className={'flex justify-between items-center'} style={{justifyContent: 'space-between'}}>
+            <h1 className={'font-semibold text-lg mb-2'}>{props.title}</h1>
             {props.children}
         </div>
-    )
-}
+    );
+};
