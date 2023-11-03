287c66f39be690a3b61c5ae72ff4e28712667bbc
Bibishan Pandey <bibishan@sireto.io>
2023-11-02 21:17:19 -0400

    Add table empty component

---
+++ client/src/atoms/TableEmptyElement/index.tsx
@@ -0,0 +1,9 @@
+export default function TableEmptyElement() {
+    return (
+        <tr className="border-b-[1px] border-b-[#303030] hover:bg-[#292929]">
+            <td className="py-5 px-4 md:px-10 text-start">
+                <span className="text-center text-white">Nothing to show</span>
+            </td>
+        </tr>
+    );
+}
