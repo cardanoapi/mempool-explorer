4e9ca2b9868d7ca9f1ef30079eb54bba44033196
Bibishan Pandey <bibishan@sireto.io>
2023-11-02 21:19:32 -0400

    Add live mempool template and update the page with template

--- client/src/app/mempool/page.tsx
+++ client/src/app/mempool/page.tsx
@@ -1,5 +1,14 @@
-import LiveMempool from '@app/organisms/LiveMempool';
+'use client';
 
-export default function Home() {
-    return <LiveMempool />;
+import React from 'react';
+
+import useMempoolAndMintEvent from '@app/lib/hooks/useMempoolAndMintEvent';
+import LiveMempool from '@app/templates/LiveMempool';
+
+function Home() {
+    const { mempoolEvent } = useMempoolAndMintEvent();
+
+    return <LiveMempool mempoolEvent={mempoolEvent} />;
 }
+
+export default React.memo(Home);
