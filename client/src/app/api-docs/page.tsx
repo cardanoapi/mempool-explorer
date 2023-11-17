4db28718eafdfee5fea1dd1d84c624a8292967fd
Nabin Pokhrel <nabin62246@gmail.com>
2023-11-17 15:54:54 -0500

    Add swagger white config

--- client/src/app/api-docs/page.tsx
+++ client/src/app/api-docs/page.tsx
@@ -1,12 +1,10 @@
 import ReactSwagger from './react-swagger';
 import {getApiDocs} from "@app/lib/swagger";
-import _default from "chart.js/dist/plugins/plugin.tooltip";
-import backgroundColor = _default.defaults.backgroundColor;
 
 export default async function IndexPage() {
     const spec = await getApiDocs();
     return (
-        <section style={{marginTop: "-50px;", backgroundColor: "white", height: "100vh;"}}>
+        <section className="mt-[-50px] bg-white h-screen">
             <ReactSwagger spec={spec}/>
         </section>
     );
