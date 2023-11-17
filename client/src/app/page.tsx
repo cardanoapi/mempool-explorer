9e92ae0888a4c52dd655796f4ce72615987e34c1
Nabin Pokhrel <nabin62246@gmail.com>
2023-11-17 15:55:05 -0500

    Remove multiple dashboard banner

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,15 +1,20 @@
+'use client';
+
+import { checkForErrorResponse } from '@app/components/loader/error';
 import DashboardBanner from '@app/molecules/DashboardBanner';
 import DashboardMempoolBanner from '@app/molecules/DashboardMempoolBanner';
 import DashboardStakePoolsBanner from '@app/molecules/DashboardStakePoolsBanner';
 import Navbar from '@app/molecules/Navbar';
+import { useEffect, useState } from 'react';
 
 export default function Home() {
+
+
     return (
         <main className="w-full min-h-screen">
             <Navbar />
-            <DashboardBanner />
+            <DashboardBanner/>
             <DashboardMempoolBanner />
-            <DashboardBanner />
             <DashboardStakePoolsBanner />
         </main>
     );
