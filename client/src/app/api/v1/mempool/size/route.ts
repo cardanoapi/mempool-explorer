04b1a3b8e1a132518b0e64fecaff45aad9fc6d09
Nabin Pokhrel <nabin62246@gmail.com>
2023-11-17 15:56:30 -0500

    Add mempool sizes api GET

--- client/src/app/api/v1/mempool/size/route.ts
+++ client/src/app/api/v1/mempool/size/route.ts
@@ -1,13 +1,13 @@
-import {NextResponse} from "next/server";
-import {getCurrentEpochInfo} from "@app/db/queries";
+import { NextResponse } from "next/server";
+import { getLatestMempoolAverageSizes } from "@app/db/queries";
 
 export const dynamic = 'force-dynamic'
 
 /**
  * @swagger
- * /api/v1/epoch/current:
+ * /api/v1/mempool/size:
  *   get:
- *     summary: Get current epoch information
+ *     summary: Get latest mempool size information
  *     responses:
  *       200:
  *         description: Success
@@ -18,10 +18,10 @@ export const dynamic = 'force-dynamic'
 export async function GET(req: Request) {
     console.log("GET: ", req.url);
     try {
-        let data = await getCurrentEpochInfo();
+        let data = await getLatestMempoolAverageSizes();
         return NextResponse.json(data)
     } catch (e: any) {
         console.log(req.url, e);
-        return NextResponse.json({error: e.name, status: !e?.errorCode ? 500 : e.errorCode})
+        return NextResponse.json({ error: e.name, status: !e?.errorCode ? 500 : e.errorCode })
     }
 }
