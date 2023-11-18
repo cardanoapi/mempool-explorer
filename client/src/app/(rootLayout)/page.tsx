24abc0848cc7d6e3313a2e24c10a7eaeabb74c58
Nabin Pokhrel <nabin62246@gmail.com>
2023-11-18 13:31:20 -0500

    Move pool distribution group to its section

--- client/src/app/(rootLayout)/page.tsx
+++ client/src/app/(rootLayout)/page.tsx
@@ -9,7 +9,6 @@ export default function Home() {
             <DashboardBanner />
             <DashboardMempoolBanner />
             <PoolDistributionGroup />
-            <DashboardStakePoolsBanner />
         </main>
     );
 }
