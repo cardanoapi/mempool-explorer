d272d674ec250db2f3b1d30948e001da771f9f2e
Nabin Pokhrel <nabin62246@gmail.com>
2023-11-19 22:02:06 -0500

    Add connect wallet function in navbar

--- client/src/molecules/Navbar/index.tsx
+++ client/src/molecules/Navbar/index.tsx
@@ -1,21 +1,73 @@
 'use client';
 
-import React from 'react';
-
+import React, { useState } from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
-
-import { Box, InputAdornment, TextField } from '@mui/material';
-
+import { Box, InputAdornment, Modal, TextField } from '@mui/material';
+import ProfileIcon from '@mui/icons-material/AccountCircle';
 import GradientButton from '@app/atoms/Button/GradientButton';
 import BrandIcon from '@app/atoms/Icon/Brand';
 import SearchIcon from '@app/atoms/Icon/Search';
 import WalletIcon from '@app/atoms/Icon/Wallet';
 import { useIsMobile } from '@app/lib/hooks/useBreakpoint';
+import { ConnectWalletList, ConnectWalletButton, useCardano } from '@cardano-foundation/cardano-connect-with-wallet';
+import Button from '@app/atoms/Button';
+import dynamic from 'next/dynamic';
+
+// const ConnectWalletList = dynamic(
+//     () =>
+//         import('@cardano-foundation/cardano-connect-with-wallet').then(
+//             (mod) => mod.ConnectWalletList
+//         ),
+//     {
+//         ssr: false,
+//     }
+// );
+
+// const useCardano = dynamic(
+//     () =>
+//         import('@cardano-foundation/cardano-connect-with-wallet').then(
+//             (mod) => mod.useCardano
+//         ),
+//     {
+//         ssr: false,
+//     }
+// );
 
 export default function Navbar() {
     const router = useRouter();
     const isMobile = useIsMobile();
+
+    const {
+        isEnabled,
+        isConnected,
+        enabledWallet,
+        stakeAddress,
+        signMessage,
+        connect,
+        disconnect
+    } = useCardano();
+
+    const onConnectWallet = () => {
+        console.log('Successfully connected!');
+        handleCloseModal();
+    };
+
+    const onDisconnectWallet = () => {
+        console.log('Successfully disconnected!');
+        disconnect();
+        handleCloseModal();
+    };
+
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const handleOpenModal = () => {
+        setIsModalOpen(true);
+    };
+
+    const handleCloseModal = () => {
+        setIsModalOpen(false);
+    };
 
     return (
         <nav className="flex items-center z-10 bg-[#0D0D0D] !backdrop-blur-3xl justify-between gap-2 h-[68px] px-4 md:h-22 md:px-10 border-b-[1px] border-[#4A4A4A]">
@@ -68,9 +120,68 @@ export default function Navbar() {
                     }}
                 />
             </Box>
-            <GradientButton size="large" startIcon={<WalletIcon />} onClick={() => {}}>
-                <span className="hidden md:block">Connect Wallet</span>
+
+            <GradientButton
+                size="large"
+                startIcon={<WalletIcon />}
+                onClick={handleOpenModal}
+            >
+                <span className="hidden md:block">
+                    {isConnected ? `${enabledWallet} Connected` : 'Connect Wallet'}
+                </span>
             </GradientButton>
+            {isConnected &&
+                <Link href="/profile">
+                    <GradientButton
+                        size="large"
+                        startIcon={<ProfileIcon />}
+                        onClick={() => { }}
+                    >
+                        <span className="hidden md:block">
+                            My Profile
+                        </span>
+                    </GradientButton>
+                </Link>
+            }
+
+            <Modal
+                open={isModalOpen}
+                onClose={handleCloseModal}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+                className="flex items-center justify-center"
+            >
+                <div className="flex flex-col">
+                    <ConnectWalletList
+                        borderRadius={48}
+                        gap={16}
+                        primaryColor="#0538AF"
+                        onConnect={onConnectWallet}
+                        customCSS={`
+                        width: 250px;
+                        border-radius: 48px !important;
+                        & > span { 
+                            padding: 13px 16px; 
+                            font-family: 'IBM Plex Mono', 'Open Sans', 'monospace';
+                            font-size: 1rem;
+                            font-weight: 500;
+                            text-align: center;
+                            width: 100%;
+                            display: flex;
+                            align-items: center;
+                            justify-content: center;
+                        }
+                        `}
+                    />
+                    {isConnected &&
+                        <Button
+                            size="large"
+                            className="flex !gap-2 !rounded-[48px] !items-center !font-ibm !font-normal !text-lg !capitalize !bg-transparent !text-white hover:!bg-gray-500"
+                            onClick={onDisconnectWallet}>Disconnect</Button>
+                    }
+                </div>
+            </Modal>
         </nav>
     );
 }
+
