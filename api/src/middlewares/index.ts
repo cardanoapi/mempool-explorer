3d0a035c4db334a21a50225ac1dbc81f46a0b772
Nabin Pokhrel <nabin62246@gmail.com>
2023-12-23 01:32:56 -0500

    Update swagger-url

--- api/src/middlewares/index.ts
+++ api/src/middlewares/index.ts
@@ -7,7 +7,7 @@ const middlewareSetup = (app: Express) => {
     app.use(morgan(':method :url :status :http-version :response-time '));
     setTimeout(() => {
         app.use(
-            '/api/swagger/docs',
+            '/api/docs',
             swaggerUi.serve,
             swaggerUi.setup(undefined, {
                 swaggerOptions: {
