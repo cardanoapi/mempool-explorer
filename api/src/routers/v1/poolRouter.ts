e1048824add28bebc12f8dec7413f4edecfbcc79
Nabin Pokhrel <nabin62246@gmail.com>
2023-12-23 01:33:06 -0500

    Add pool routers

--- api/src/routers/v1/poolRouter.ts
+++ api/src/routers/v1/poolRouter.ts
@@ -15,4 +15,21 @@ router.get('/timing', async (req, res) => {
     res.json(response);
 });
 
+// TODO : Replace the redudnant declaration of router get endpoints
+router.get('/:poolId/epoch', async (req, res) => {
+    const response = await poolController.getPoolEpochInfo(req.params.poolId);
+    res.json(response);
+});
+
+router.get('/:poolId/transactions', async (req, res) => {
+    const response = await poolController.getPoolTransactions(req.params.poolId, Number.parseInt(req.query.pageNumber as string));
+    res.json(response);
+});
+
+router.get('/:poolId/transaction-timing', async (req, res) => {
+    const response = await poolController.getPoolTransactionTiming(req.params.poolId);
+    res.json(response);
+});
+
+
 export default router;
