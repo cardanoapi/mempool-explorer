6f29d7dd6f6f0260fe42532cda271e2aa4461b35
Nabin Pokhrel <nabin62246@gmail.com>
2023-12-25 18:10:38 -0500

    Remove db quries from pool timing

--- client/src/app/api/v1/pool/distribution/route.ts
+++ client/src/app/api/v1/pool/distribution/route.ts
@@ -1,20 +1,9 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 import environments from '@app/configs/environments';
-import { getPoolDistributionGroup } from '@app/db/queries';
 
 export const dynamic = 'force-dynamic';
 
-const handler = async (req: NextRequest, res: NextResponse) => {
-    console.log('GET: ', req.url);
-    try {
-        let data = await getPoolDistributionGroup();
-        return data;
-    } catch (e: any) {
-        console.log(req.url, e);
-        return { error: e.name, status: !e?.errorCode ? 500 : e.errorCode };
-    }
-};
 
 /**
  * @swagger
--- client/src/app/api/v1/pool/timing/route.ts
+++ client/src/app/api/v1/pool/timing/route.ts
@@ -2,21 +2,8 @@ import { NextResponse } from 'next/server';
 import { NextRequest } from 'next/server.js';
 
 import environments from '@app/configs/environments';
-import { getAveragePoolConfirmTimeForLastSevenDays } from '@app/db/queries';
 
 export const dynamic = 'force-dynamic';
-
-const handler = async (req: NextRequest, res: NextResponse) => {
-    console.log('GET: ', req.url);
-    try {
-        let data = await getAveragePoolConfirmTimeForLastSevenDays();
-        console.log('Pool Timing Handler: ', data);
-        return data;
-    } catch (e: any) {
-        console.log(req.url, e);
-        return { error: e.name, status: !e?.errorCode ? 500 : e.errorCode };
-    }
-};
 
 /**
  * @swagger
