03ecf36ba9a198dd6b79bce72593685d29f641aa
Nabin Pokhrel <nabin62246@gmail.com>
2023-12-25 18:06:00 -0500

    Remove db quries from mempool size

--- client/src/app/api/v1/mempool/size/route.ts
+++ client/src/app/api/v1/mempool/size/route.ts
@@ -1,21 +1,8 @@
 import { NextRequest, NextResponse } from 'next/server.js';
 
 import environments from '@app/configs/environments';
-import { getLatestMempoolAverageSizes } from '@app/db/queries';
 
 export const dynamic = 'force-dynamic';
-
-const handler = async (req: NextRequest, res: NextResponse) => {
-    console.log('GET: ', req.url);
-    try {
-        let data = await getLatestMempoolAverageSizes();
-        console.log('Mempool Size Handler: ', data);
-        return data;
-    } catch (e: any) {
-        console.log(req.url, e);
-        return { error: e.name, status: !e?.errorCode ? 500 : e.errorCode };
-    }
-};
 
 /**
  * @swagger
