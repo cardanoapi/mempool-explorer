d6fba7dfe03662ef98d6068bca7222b724b64569
Bibishan Pandey <bibishan@sireto.io>
2024-04-24 23:18:49 -0400

    Fix axios API URL environment variable

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -1,16 +1,16 @@
+import axios from 'axios';
 
-import axios from 'axios'
+import environments from '@app/configs/environments';
 
-const BASE_URL = process.env.NEXT_PUBLIC_API_URL
-
+const BASE_URL = environments.API_URL;
 
 const api = axios.create({
     baseURL: BASE_URL,
     headers: {
         'Content-Type': 'application/json',
-        'Accept': 'application/json'
+        Accept: 'application/json'
     },
-    timeout: 30000,
+    timeout: 30000
 });
 
 export default api;
