3d4dbae6390b4fe909614ffff1f2087b72ae4390
Bibishan Pandey <bibishan@sireto.io>
2024-04-24 21:40:07 -0400

    Add dark mode theme for material ui

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,6 +1,10 @@
+'use client';
+
 import Script from 'next/script';
 import NextTopLoader from 'nextjs-toploader';
 
+import { CssBaseline } from '@mui/material';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -12,10 +16,19 @@ export default function RootLayout({ children }: { readonly children: React.Reac
     return (
         <html lang="en">
             <body className="!bg-black !text-white">
-                <NextTopLoader showSpinner={false} color="#E6E6E6" />
-                <Navbar />
-                <ToastContainer position="bottom-right" theme="dark" autoClose={3000} newestOnTop closeOnClick pauseOnFocusLoss pauseOnHover limit={8} />
-                {children}
+                <ThemeProvider
+                    theme={createTheme({
+                        palette: {
+                            mode: 'dark'
+                        }
+                    })}
+                >
+                    <CssBaseline />
+                    <NextTopLoader showSpinner={false} color="#E6E6E6" />
+                    <Navbar />
+                    <ToastContainer position="bottom-right" theme="dark" autoClose={3000} newestOnTop closeOnClick pauseOnFocusLoss pauseOnHover limit={8} />
+                    {children}
+                </ThemeProvider>
             </body>
             {/* <!-- Google tag (gtag.js) --> */}
             <Script id="google-analytics-script" strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=G-MG10RWWQ1Y`} />
